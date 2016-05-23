f434e49f3abb57d3e4100d5fd017495cbde9c466
Brad Boehmke <bradleyboehmke@gmail.com>
2016-05-23 06:57:10 -0400

    save

--- quickplot_image.R
+++ quickplot_image.R
@@ -1,27 +1,9 @@
 
 
-par(bg = "#fdfdfd")
-plot(x = mtcars$wt, y = mtcars$mpg, xlab = NA, ylab = NA)
-
-par(bg = "#fdfdfd")
-barplot(height = BOD$demand, names.arg = BOD$Time)
-
-par(bg = "#fdfdfd")
-plot(x = pressure$temperature, y = pressure$pressure, type = "l", xlab = NA, ylab = NA)
-
-par(bg = "#fdfdfd")
-plot(factor(mtcars$cyl), mtcars$mpg)
-
 par(mfrow=c(2,2), mar=c(3,5,2,2), las=1, bty="n")
-par(bg = "#fdfdfd")
+par(bg = "#EEEEEE")
 plot(x = mtcars$wt, y = mtcars$mpg, xlab = NA, ylab = NA)
-
-par(bg = "#fdfdfd")
 barplot(height = BOD$demand, names.arg = BOD$Time)
-
-par(bg = "#fdfdfd")
 plot(x = pressure$temperature, y = pressure$pressure, type = "l", xlab = NA, ylab = NA)
-
-par(bg = "#fdfdfd")
 plot(factor(mtcars$cyl), mtcars$mpg)
 
